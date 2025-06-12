8da4a791e2f234b1f3a9324a7cb1e1f56f981379
Chi <cragbasi@gmail.com>
2025-06-12 03:13:57 -0600

    Project 13 v0

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,6 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
 
-const {
-  login,
-  signup,
-  createUser,
-  getCurrentUser,
-  updateProfile,
-  getUsers,
-  getUserByID,
-} = require("../controllers/users");
-
 router.use("/items", clothingItemRouter);
 router.use("/", userRouter);
 // router.post("/users", userRouter);
