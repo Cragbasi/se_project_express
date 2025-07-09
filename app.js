8b9e0d9d2020c065f19cd289acfaf9c29b5074c0
Chi <cragbasi@gmail.com>
2025-07-09 13:22:27 -0600

    Project 15 v1  addressed reviewers comments

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(requestLogger);
 
-// app.get("/crash-test", () => {
-//   setTimeout(() => {
-//     throw new Error("Server will crash now");
-//   }, 0);
-// });
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
 app.use("/", routes);
 
 const handleNonExistentRoute = (req, res, next) => {
--- utils/ecosystem.config.js
+++ utils/ecosystem.config.js
@@ -4,10 +4,16 @@ module.exports = {
       name: "app",
       script: "app.js",
       watch: true,
-      ignore_watch: ["node_modules", "logs"],
-      env: {
-        NODE_ENV: "production",
-      },
     },
   ],
+  deploy: {
+    production: {
+      user: "cragbasi",
+      host: "your-server-ip",
+      ref: "origin/main",
+      repo: "git@github.com:cragbasi/se_project_express",
+      path: "/home/cragbasi/se_project_express",
+      "post-deploy": "npm install && pm2 restart app",
+    },
+  },
 };
